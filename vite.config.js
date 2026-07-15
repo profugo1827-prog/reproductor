07dbc3eec0dc997377eed887a1fbf65711d225a0
profugo1827-prog <profugo1827@gmail.com>
2026-07-15 09:47:38 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
         background_color: '#121212',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: '/',
+        start_url: '/reproductor/',
         icons: [
           { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png' },
